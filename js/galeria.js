6bb6cc91bb3d505072d2e9920f52d0acd9ed297d
Hamza-Elhardi <kissmizo.com@gmail.com>
2026-02-16 14:30:39 +0100

    Add files via upload
    add Mi Pajina Web

---
+++ js/galeria.js
@@ -0,0 +1,11 @@
+
+
+
+// cambiar alguna de las opciones predeterminadas de Lightbox
+
+
+    lightbox.option({
+      'resizeDuration': 200,
+      'wrapAround': true,
+      'showImageNumberLabel': false
+    })
---
+++ js/scripts.js
@@ -0,0 +1,75 @@
+// incorporar archivo Xml en la pajina index.html
+
+// funtion de cargar fichier XML son datos de section de Articolos
+function cargarArticolos(){
+
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+
+        if(this.readyState == 4 && this.status == 200){
+            mostrarArticolos(this);
+        }
+    }
+
+
+
+    request.open('GET' , 'data/noticias.xml' , true);
+    request.send();
+
+
+}
+
+
+// function de mostrar info 
+function mostrarArticolos(elem){
+    // creamos una varaible donde almacenamos response
+    let archivo = elem.responseXML;
+    
+
+    // llamamos a los elementos que hay en archivo XML
+    let titlexml = archivo.getElementsByTagName('titulo');
+    let descriptionxml = archivo.getElementsByTagName('description');
+    let fechaxml = archivo.getElementsByTagName('fecha');
+
+
+    // llamamos a los elemontos de section noticias
+    let title = document.getElementById('noticias').querySelectorAll('.title')
+    let parragrafo = document.getElementById('noticias').querySelectorAll('.parragrafo');
+    let fecha = document.getElementById('noticias').querySelectorAll('.fecha');
+    
+ 
+
+       for(let i = 0 ; i<titlexml.length ; i++){
+              
+
+            title[i].innerHTML = "<strong>" +titlexml[i].childNodes[0].nodeValue +"</strong>";
+            parragrafo[i].innerHTML = descriptionxml[i].childNodes[0].nodeValue;
+            fecha[i].innerHTML = fechaxml[i].childNodes[0].nodeValue;
+
+
+     }
+
+
+ 
+            
+
+
+
+
+            
+       
+
+    
+      
+  
+  
+       
+}
+
+cargarArticolos();
+
+
+
+
+
+
